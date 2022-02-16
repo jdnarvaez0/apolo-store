cf24f8ce55d15fed1503e52ea6b2687c27c0fcc8
navidev0 <juand.ns07@gmail.com>
2022-02-16 12:46:58 -0300

    mejoras en CartWidget, Navbar

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,8 +1,36 @@
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { CartIcon } from './icons/index.js'
+import { cartContext } from '../contex/CartProvider'
 
 const CartWidget = () => {
+  const { cart } = useContext(cartContext)
+
+  console.log(cart)
+  const totalProductos = cart.reduce(
+    (counter, item) => counter + item.count,
+    0
+  )
+
   return (
     <div>
-      Cart <span>0</span> 
+      <Link to='/cart'>
+        <button
+          type='button'
+          className='mr-2 text-md text-white text-4xl relative'
+        >
+          <span className='w-4 h-4 rounded-full absolute right-2 leading text-xs bg-red-500'>
+            {totalProductos}
+          </span>
+          <svg
+            fill='currentColor'
+            viewBox='0 0 1792 1792'
+            className='text-black h-9 w-9'
+          >
+            <CartIcon />
+          </svg>
+        </button>
+      </Link>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,24 @@
-import { CartIcon } from './icons/index.js'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import CartWidget from './CartWidget'
 
 const Navbar = () => {
   return (
     <header className='text-gray-600 body-font pt-4'>
       <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
         <nav className='md:ml-auto md:mr-auto flex flex-wrap items-center justify-center font-regular text-lg'>
-          <Link to='/' className='mr-8 hover:text-gray-800 hover:underline'>Store</Link>
-          <Link to='/about' className='mr-8 hover:text-gray-800 hover:underline'>About</Link>
-          <span className='inline-block w-6 mr-2 text-center'>
-            <CartIcon />
-          </span>
-          <Link to='/cart' className='mr-8 hover:text-gray-800 hover:underline'>Cart</Link>
+          <Link to='/' className='mr-8 hover:text-gray-800 hover:underline'>
+            Store
+          </Link>
+          <Link
+            to='/about'
+            className='mr-8 hover:text-gray-800 hover:underline'
+          >
+            About
+          </Link>
+            <CartWidget />
+          <Link to='/cart' className='mr-8 hover:text-gray-800 hover:underline'>
+            Cart
+          </Link>
         </nav>
       </div>
     </header>
